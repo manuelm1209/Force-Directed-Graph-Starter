46dff221418fbb93bc577e9f661c311bf45ef9ec
Manuel Montoya <manuelm1209@hotmail.com>
2022-11-26 14:01:49 -0500

    child and leaf nodes

--- data.js
+++ data.js
@@ -1,6 +1,61 @@
-export const nodes = [{ id: "Alice" }, { id: "Bob" }, { id: "Carol" }];
+export const nodes = [];
+export const links = [];
 
-export const links = [
-  { source: 0, target: 1 }, // Alice → Bob
-  { source: 1, target: 2 }, // Bob → Carol
-];
+const MAIN_NODE_SIZE = 50;
+const CHILD_NODE_SIZE = 20;
+const LEAF_NODE_SIZE = 7;
+const DEFAULT_DISTANCE = 150;
+const LEAF_NODE_DISTANCE = 20;
+export const MANY_BODY_STRENGTH = -50;
+
+const addMainNode = (mainNode) => {
+  mainNode.size = MAIN_NODE_SIZE;
+  nodes.push(mainNode);
+};
+
+const addChileNode = (
+  parentNode,
+  childNode,
+  size = CHILD_NODE_SIZE,
+  distance = DEFAULT_DISTANCE
+) => {
+  childNode.size = size;
+  nodes.push(childNode);
+  links.push({ source: parentNode, target: childNode, distance });
+};
+
+const parentNode = { id: "Arts Web" };
+addMainNode(parentNode);
+
+const childNode = { id: "Community Vision" };
+addChileNode(parentNode, childNode);
+
+for (let i = 0; i < 20; i++) {
+  addChileNode(childNode, { id: "" }, LEAF_NODE_SIZE, LEAF_NODE_DISTANCE);
+}
+
+addChileNode(parentNode, { id: "Silicon Valley Creates" });
+
+// export const nodes = [
+//   {
+//     founder: "",
+//     company: "",
+//     founded_year: "",
+//     multiplier_effect_type: "",
+//     logo_url: "",
+//   },
+//   {
+//     founder: "",
+//     company: "",
+//     founded_year: "",
+//     multiplier_effect_type: "",
+//     logo_url: "",
+//   },
+//   {
+//     founder: "",
+//     company: "",
+//     founded_year: "",
+//     multiplier_effect_type: "",
+//     logo_url: "",
+//   },
+// ];
--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const { select, forceSimulation, forceManyBody, forceLink, forceCenter } = d3;
 
-import { nodes, links } from "./data.js";
-
-// const nodes = [{ id: "Alice" }, { id: "Bob" }, { id: "Carol" }];
-
-// const links = [
-//   { source: 0, target: 1 }, // Alice → Bob
-//   { source: 1, target: 2 }, // Bob → Carol
-// ];
+import { nodes, links, MANY_BODY_STRENGTH } from "./data.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -17,16 +10,12 @@ const svg = select("#container").attr("width", w).attr("height", h);
 const centerx = window.innerWidth;
 
 const simulation = forceSimulation(nodes)
-  .force("charge", forceManyBody())
-  .force("link", forceLink(links))
+  .force("charge", forceManyBody().strength(MANY_BODY_STRENGTH))
+  .force(
+    "link",
+    forceLink(links).distance((link) => link.distance)
+  )
   .force("center", forceCenter(w / 2, h / 2));
-
-const circles = svg
-  .selectAll("circle")
-  .data(nodes)
-  .enter()
-  .append("circle")
-  .attr("r", 10);
 
 const lines = svg
   .selectAll("lines")
@@ -35,8 +24,28 @@ const lines = svg
   .append("line")
   .attr("stroke", "black");
 
+const circles = svg
+  .selectAll("circle")
+  .data(nodes)
+  .enter()
+  .append("circle")
+  .attr("fill", "grey");
+
+const text = svg
+  .selectAll("text")
+  .data(nodes)
+  .enter()
+  .append("text")
+  .attr("text-anchor", "middle")
+  .attr("alignment-baseline", "middle")
+  .text((node) => node.id);
+
 simulation.on("tick", () => {
-  circles.attr("cx", (node) => node.x).attr("cy", (node) => node.y);
+  circles
+    .attr("cx", (node) => node.x)
+    .attr("cy", (node) => node.y)
+    .attr("r", (node) => node.size);
+  text.attr("x", (node) => node.x).attr("y", (node) => node.y);
   lines
     .attr("x1", (link) => link.source.x)
     .attr("y1", (link) => link.source.y)
